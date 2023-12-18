cb1d543d6bff9f9b1eea86e62a05ba9578098ebb
David Ragones <dragones@gmail.com>
2023-12-18 10:28:43 -0800

    clean up.  add file parameter.

--- transcribe.py
+++ transcribe.py
@@ -1,43 +1,21 @@
 import sys
-sys.path.insert(1, './whisper-mlx/')
-#sys.path.append('./whisper-mlx/whisper/')
-
+sys.path.insert(1, '../mlx-examples/whisper/')
 
 import whisper
+import argparse
 import os
 
-#speech_file = "./NovemberBoarddMeeting.m4a"
-#speech_file = "~dragones/.cache/whisper/alice.mp3"
-#text = whisper.transcribe(speech_file)["text"]
-#print(text)
+def main():
+    parser = argparse.ArgumentParser(description='Transcribe audio file')
+    parser.add_argument('-f', '--file', type=str, help='The file to transcribe')
+    args = parser.parse_args()
 
-audio_file = os.path.join(
-    os.path.expanduser("~"),
-#    ".cache/whisper/alice.mp3",
-    "Code/mlx-examples/whisper/NovemberBoardMeeting.m4a",
-)
+    if not os.path.exists(args.file):
+        print("audio file not found: ", args.file)
+        exit()
 
-# print current folder name
-# print(os.getcwd())
+    result = whisper.transcribe(model="base", audio=args.file, fp16=False)
+    print(result["text"])
 
-# print out all files in current folder
-# print(os.listdir(os.getcwd()))
-
-if not os.path.exists(audio_file):
-    print("audio file not found: ", audio_file)
-    exit()
-
-#model = whisper.load_model("small")
-#result = whisper.transcribe(audio_file, fp16=False)
-result = whisper.transcribe(model="tiny", audio=audio_file, fp16=False)
-print(result["text"])
-
-# Save as a TXT file with hard line breaks
-#output_directory = "./transcripts/"
-
-# check if output_directory exists.  if not, create it
-#if not os.path.exists(output_directory):
-#    os.makedirs(output_directory)
-
-#txt_writer = get_writer("txt", output_directory)
-#txt_writer(result, "NovemberBoardMeeting.m4a")
+if __name__ == '__main__':
+    main()
